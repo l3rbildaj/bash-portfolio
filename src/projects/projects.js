04525e633c60c0572595ded7a90b2b5629df8818
elmofaker222 <elmofaker222@gmail.com>
2023-03-08 12:57:47 +0100

    hope this fixes it

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,25 +1,31 @@
+import chatting from "../assets/chatting-app.png";
+import ecom from "../assets/e-com.png";
+import todo from "../assets/todo.png";
+import stage from "../assets/stage-management.png";
+
+
 export const projects = [
     {
         name:"e-commerce_web_app",
-        img:"e-com.png",
+        img:ecom,
         description:" Laravel-based e-commerce web project has authentication, a shopping cart, and an admin dashboard. Users can log in securely, add items to their cart, and admins can manage inventory and orders. this Laravel-based e-commerce web project provides a user-friendly and secure online shopping experience with robust features for both users and administrators.",
         link:"#"
     },
     {
         name:"chatting_app",
-        img:"chatting-app.png",
+        img:chatting,
         description:"This Django-based chatting and todo app features real-time chatting via websockets, authentication, profile management, and a to-do list for managing tasks.Django-based chatting and todo app is a robust web application that provides users with a real-time chatting experience and helps them manage their to-do list effectively.",
         link:"#"
     },
     {
         name:"stage_management_Desktop_app",
-        img:"stage-management.png",
+        img:stage,
         description:"This Python Tkinter-based app is a desktop application that utilizes PyMongo to perform CRUD (Create, Read, Update, Delete) operations on a MongoDB database. The app features a user-friendly graphical interface created with Tkinter that allows users to interact with the database easily.",
         link:"#"
     },
     {
         name:"todo",
-        img:"todo.png",
+        img:todo,
         description:"The Todo project is a web application developed using React, Redux, and Next UI on the frontend and Django (DRF) on the backend. The application provides users with the ability to create, update, and delete tasks to manage their daily activities. The authentication feature ensures that only authenticated users can access the application's functionality.",
         link:"#"
     }
