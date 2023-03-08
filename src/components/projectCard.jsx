6c55b61b9076d49a27c685d7dc5b6c18b1bf3289
elmofaker222 <elmofaker222@gmail.com>
2023-03-08 12:39:54 +0100

    building the assets

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -17,7 +17,7 @@ export default function ProjectCard({name, picture, description, github_link}) {
         <div className="relative transform transition duration-500 hover:shadow-2xl hover:shadow-white hover:scale-95 hover:border-solid border-dashed border-white border-2 w-screen md:w-1/3 p-3">
             <div className="flex justify-center items-center h-full m-auto">
 
-            <img className="mx-auto m-4" src={"src/assets/"+picture} alt="" />
+            <img className="mx-auto m-4" src={"assets/"+picture} alt="" />
             </div>
             <div className="opacity-0 p-5  absolute gap-5 flex-col items-center  transform transition duration-100 hover:opacity-100  hover:flex hover:bg-gray-800 hover:bg-opacity-80 inset-0 w-full h-full">
                 <h1 className="text-xl">#{name}</h1>
